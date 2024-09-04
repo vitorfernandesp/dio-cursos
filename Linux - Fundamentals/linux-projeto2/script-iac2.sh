ffb93d44c8194a9487793d8dbcd5a30be4a2a3aa
Vitor Fernandes <169093382+vitorfernandesp@users.noreply.github.com>
2024-09-04 19:24:47 -0300

    Update script-iac2.sh

--- Linux - Fundamentals/linux-projeto2/script-iac2.sh
+++ Linux - Fundamentals/linux-projeto2/script-iac2.sh
@@ -10,7 +10,7 @@ apt-get install unzip -y
 echo "Baixando e copiando os arquivos da aplicação..."
 
 cd /tmp
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+wget linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
 cd linux-site-dio-main
 cp -R * /var/www/html/
